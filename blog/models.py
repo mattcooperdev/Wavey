a46ea13ffb16f31b8f322f6aa7494204cbe85b82
YesCoops <coopandmgc@gmail.com>
2022-05-05 17:21:46 +0000

    Create post detail class and linked up to relevant page

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,9 @@ class Post(models.Model):
     '''
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -34,7 +36,9 @@ class Comment(models.Model):
     '''
     Model used to track comment.
     '''
-    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+        related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'), #first slug is path converter
 ]
