9ae62a7c98a59156816a49c313a3feaac6fbc619
YesCoops <coopandmgc@gmail.com>
2022-05-06 12:05:30 +0000

    Add Allauth and styled login page

--- wavey/urls.py
+++ wavey/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog_urls'),
+    path('accounts/', include('allauth.urls'))
 ]
