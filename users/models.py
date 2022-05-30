27fe85d9a0d9fa41f71221a35b611a13e95891b7
mattcooperdev <coopandmgc@gmail.com>
2022-05-30 19:58:07 +0000

    [Build] Create Profile model and update profile html

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    '''Django DB model for profile creation '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50, blank=True, null=True)
+    profile_image = CloudinaryField('image', default='')
+    bio = models.TextField(max_length=800, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    social_youtube = models.URLField(max_length=200, blank=True, null=True)
+    social_website = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
