471d35a3dce1c279a1be0e9104c3be7864fe067d
mattcooperdev <mattcooperdev@gmail.com>
2022-11-19 11:41:29 +0000

    Updated config to allow Render deployment

--- wavey/settings.py
+++ wavey/settings.py
@@ -33,6 +33,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['wavey-django.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+	ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
